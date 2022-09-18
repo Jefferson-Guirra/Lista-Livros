16c44bfb5092afe4ed48e38d2139017b729511ad
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-09-17 21:39:59 -0300

    Lista de Tarefas completa

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { ImageBackground } from 'react-native'
+import * as C from './styles'
+import Main from './src/pages/main'
+import AddBooks from './src/pages/AddBooks'
+import {Data} from './src/pages/main'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export type RootStackParamList = {
+  Main: undefined
+  AddBooks: { book:Data } | undefined
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator<RootStackParamList>()
+export default function App() {
+  return (
+    <C.Container>
+      
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Main" component={Main} />
+            <Stack.Screen name="AddBooks" component={AddBooks} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    </C.Container>
+  )
+}
